d5dbc37d7b532ce7404f1be32ae4f6fa48c873b9
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-05-11 11:53:52 +0100

    slider and products are being displayed

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,48 @@
 <template>
   <div id="app">
     <h1>My Shop</h1>
-    <product-list :maximum="maximum" :products="products"></product-list>
+    <price-slider :sliderStatus="sliderStatus"></price-slider>
+    <product-list :maximum="maximum" :products="products" @add="addItem"></product-list>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import ProductList from "./components/ProductList";
+import PriceSlider from "./components/PriceSlider";
 
 export default {
   name: "app",
   data: function() {
     return {
       maximum: 99,
+      sliderStatus: true,
+      cart: [],
       products: null
     };
   },
+  methods: {
+    addItem: function(product) {
+      var whichProduct;
+      var existing = this.cart.filter(function(item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    }
+  },
   components: {
     FontAwesomeIcon,
-    ProductList
+    ProductList,
+    PriceSlider
   },
   mounted: function() {
     fetch("https://hplussport.com/api/products/order/price")
---
+++ src/components/PriceSlider.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition
+    name="custom"
+    enter-active-class="animated fadeInDown"
+    leave-active-class="animated slideOutRight"
+  >
+    <div v-if="sliderStatus">
+      <div class="align-items-center" :class="sliderState">
+        <label :class="labelArr" for="formMax">max</label>
+        <input
+          type="text"
+          id="formMax"
+          class="form-control mx-2"
+          :style="{'width': inputWidth + 'px', textAlign:'center'}"
+          v-model="maximum"
+        >
+        <input type="range" class="custom-range" min="0" max="200" v-model="maximum">
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "price-slider",
+  props: ["sliderStatus"],
+  computed: {
+    sliderState: function() {
+      return this.sliderStatus ? "d-flex" : "d-none";
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
